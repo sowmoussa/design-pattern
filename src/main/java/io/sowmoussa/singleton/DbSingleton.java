209544959f52e679aded5278febef6ba3bd3d980
Moussa SOW <moussa4.sow@gmail.com>
2021-11-29 18:52:30 +0100

    [Design Pattern](S): Init Singleton adding Database

--- src/main/java/io/sowmoussa/singleton/DbSingleton.java
+++ src/main/java/io/sowmoussa/singleton/DbSingleton.java
@@ -1,14 +1,45 @@
 package io.sowmoussa.singleton;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DbSingleton {
     // volatile ensure that instance will remain a singleton through any of the changes inside  of the JVM
     private static volatile DbSingleton instance = null;
+    private static volatile Connection conn = null;
 
     private DbSingleton() {
+        try {
+            DriverManager.registerDriver(new org.apache.derby.jdbc.EmbeddedDriver());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        if(conn != null) {
+            throw new RuntimeException("Use getConnection() method to create");
+        }
+
         // ensure that nobody will use Reflection
         if(instance != null) {
             throw new RuntimeException("Use getInstance() method to create");
         }
+    }
+
+    public Connection getConnection() {
+        if (conn == null) {
+            synchronized (DbSingleton.class) {
+                if(conn == null) {
+                    try {
+                        String dbUrl = "jdbc:derby:memory:codejava/webdb;create=true";
+                        conn = DriverManager.getConnection(dbUrl);
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        return conn;
     }
 
     public static DbSingleton getInstance() {
--- src/main/java/io/sowmoussa/singleton/DbSingletonMain.java
+++ src/main/java/io/sowmoussa/singleton/DbSingletonMain.java
@@ -1,11 +1,34 @@
 package io.sowmoussa.singleton;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DbSingletonMain {
     public static void main(String[] args) {
         DbSingleton instance = DbSingleton.getInstance();
+        long beforeCreated = 0;
+        long afterCreated = 0;
         System.out.println(instance);
 
-        DbSingleton anotherInstance = DbSingleton.getInstance();
-        System.out.println(anotherInstance);
+        beforeCreated = System.currentTimeMillis();
+        Connection conn = instance.getConnection();
+        afterCreated = System.currentTimeMillis();
+        System.out.println(afterCreated - beforeCreated);
+
+        Statement statement;
+        try {
+            statement = conn.createStatement();
+            int count = statement.executeUpdate("CREATE TABLE Address (ID INT, STreetName VARCHAR(20), City VARCHAR (20))");
+            System.out.println("Table Address created");
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        beforeCreated = System.currentTimeMillis();
+        conn = instance.getConnection();
+        afterCreated = System.currentTimeMillis();
+        System.out.println(afterCreated - beforeCreated);
     }
 }

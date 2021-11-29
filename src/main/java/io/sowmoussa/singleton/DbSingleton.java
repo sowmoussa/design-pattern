d067ff272a947b808ce5109b826bbec13ec07e8f
Moussa SOW <moussa4.sow@gmail.com>
2021-11-29 18:05:28 +0100

    [Design Pattern](S): Init Singleton with Lazy Loading

--- src/main/java/io/sowmoussa/singleton/DbSingleton.java
+++ src/main/java/io/sowmoussa/singleton/DbSingleton.java
@@ -1,13 +1,15 @@
 package io.sowmoussa.singleton;
 
 public class DbSingleton {
-    private static DbSingleton instance = new DbSingleton();
+    private static DbSingleton instance = null;
 
-    private DbSingleton() {
-
-    }
+    private DbSingleton() {}
 
     public static DbSingleton getInstance() {
+        // Lazy Loading will help your app to come up quicker
+        if (instance == null) {
+            instance = new DbSingleton();
+        }
         return instance;
     }
 }

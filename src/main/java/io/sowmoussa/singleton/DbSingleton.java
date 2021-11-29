f34cc15abc84303d6724fe232b959c2c38d3e9d2
Moussa SOW <_sowmoussa@MacBook-Pro-de-Moussa.local>
2021-11-29 17:54:36 +0100

    Init Singleton PAttern

---
+++ src/main/java/io/sowmoussa/singleton/DbSingleton.java
@@ -0,0 +1,13 @@
+package io.sowmoussa.singleton;
+
+public class DbSingleton {
+    private static DbSingleton instance = new DbSingleton();
+
+    private DbSingleton() {
+
+    }
+
+    public static DbSingleton getInstance() {
+        return instance;
+    }
+}
---
+++ src/main/java/io/sowmoussa/singleton/DbSingletonMain.java
@@ -0,0 +1,13 @@
+package io.sowmoussa.singleton;
+
+public class DbSingletonMain {
+    public static void main(String[] args) {
+        DbSingleton instance = DbSingleton.getInstance();
+        System.out.println(instance);
+
+        DbSingleton anotherInstance = DbSingleton.getInstance();
+        System.out.println(anotherInstance);
+
+
+    }
+}
